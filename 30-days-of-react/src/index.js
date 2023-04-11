105c35f1e0bb197805efe2b38fe38227af4ec90e
Carolina Silva <carol_paiva@outlook.com>
2023-04-11 16:50:10 -0300

    add exercicios dia05 nivel01

--- 30-days-of-react/src/index.js
+++ 30-days-of-react/src/index.js
@@ -1,99 +1,149 @@
-// index.js
 import React from 'react'
 import ReactDOM from 'react-dom'
-// To get the root element from the HTML document
-import foto from './img/img.png'
 
-import logos from './img/logos.png';
+import userimagem from '../src/img/img.png' 
 
-// JSX element, header
-const welcome = 'Welcome to 30 Days Of React'
-const title = 'Getting Started React'
-const subtitle = 'JavaScript Library'
-const author = {
-  firstName: 'Asabeneh',
-  lastName: 'Yetayeh',
+const showDate = (time) => {
+  const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ]
+
+  const month = months[time.getMonth()].slice(0,3)
+  const year = time.getFullYear()
+  const date = time.getDate()
+  return `${month} ${date}, ${year}`
+
 }
-const date = 'Oct 2, 2020'
 
-// JSX element, header
-const header = (
-  <header>
-    <div className='header-wrapper'>
-      <h1>{welcome}</h1>
-      <h2>{title}</h2>
-      <h3>{subtitle}</h3>
-      <p>
-        Instructor: {author.firstName} {author.lastName}
-      </p>
-      <small>Date: {date}</small>
-    </div>
-  </header>
-)
+const Header = ({
+  data: {
+    welcome, 
+    title, 
+    subtitle, 
+    author: {firstName, lastName}, 
+    date,
+  },
+}) => {
+  return(
+    <header>
+      <div className='header-wrapper'>
+        <h1>{welcome}</h1>
+        <h2>{title}</h2>
+        <h3>{subtitle}</h3>
+        <p>
+          {firstName} {lastName}
+        </p>
+        <small>{showDate(date)}</small>
+      </div>
+    </header>
+  )
+}
 
-// JSX element, main
-const techs = ['HTML', 'CSS', 'Js', 'Sass', 'React', 'Redux', 'Node', 'MongoDB', 'Python', 'Mysql', 'Docker', 'Git']
-const techsFormatted = techs.map((tech) => <li>{tech}</li>)
+const TechList = ({techs}) => {
+  const techList = techs.map((tech) => <li key ={tech}>{tech}</li>)
+  return techList
+}
 
-const user = (
-  <div className='card-user'>
-    <img src={foto} alt='imagem ' />
-    <h3>Your Name</h3>
-    <p>Junior Developer, Brazil</p>
-    <h3>Skills</h3>
-    <ul>{techsFormatted}</ul>
-    <spam>Joined on Aug 30, 2020</spam>
+const UserCard = ({user: {firstName, lastName, image}}) =>(
+  <div className='user-card'>
+    <img src={image} alt={firstName}/>
+    <h2>
+      {firstName}
+      {lastName}
+    </h2>
   </div>
 )
 
-// JSX element, main
-const main = (
+const Button = ({text, onClick, style }) => (
+  <button style={style} onClick={onClick}>
+    {text}
+  </button>
+)
+const buttonStyle = {
+  backgroundColor: '#ff55ff',
+  padding: 10,
+  border: 'none',
+  borderRadius: 5,
+  margin: 3,
+  cursor: 'pointer',
+  fontSize: 18,
+  color: 'white',
+
+}
+
+const Main = ({ user, techs, greetPeople, handleTime}) => (
   <main>
     <div className='main-wrapper'>
-      {user}
-    </div>
-    <div className='front-end-tec'>
-      <h1>Front End:</h1>
-      <img src={logos} alt='log html css js'/>
+      <p>Prerequisite to get started react.js:</p>
+      <ul>
+        <TechList techs={techs} />
+      </ul>
+      <UserCard user={user}/>
+      <Button text='Greet People' onClick={greetPeople} style={buttonStyle} />
+      <Button text='Show Time' onClick={handleTime} style={buttonStyle} />
+
     </div>
   </main>
 )
 
-const copyRight = 'Copyright 2020'
-
-// exercicios
-const subscribe = (
-  <div className='sub'>
-    <h1>Subscribe</h1>
-    <p>Sing up with your email address to receive news and updates.</p>
-    <div>
-    <input type='text' placeholder='First name'></input>
-    <input type='text'placeholder='Last name' ></input>
-    <input type='email' placeholder='your best email!'></input>
-    </div>
-    <button>Subscribe</button>
-  </div>
-)
-
-// JSX element, footer
-const footer = (
+const Footer = ({copyRigth}) =>(
   <footer>
     <div className='footer-wrapper'>
-      <p>{copyRight}</p>
+      <p>Copyright {copyRigth.getFullYear()}</p>
     </div>
   </footer>
 )
 
-// JSX element, app
-const app = (
-  <div className='app'>
-    {header}
-    {main}
-    {subscribe}
-    {footer}
-  </div>
-)
+const App = () =>{
+  const data ={
+    welcome: 'Seja bem-vindo',
+    title: '30 dias de React',
+    subtitle:'Biblioteca javascript',
+    author:{
+      firstName: 'Carolina',
+      lastName:'Silva',
+    },
+    date: new Date(),
+  }
+  const date = new Date()
+  const techs = ['HTML', 'CSS', 'JavaScript']
+
+  const user = {...data.author, image: userimagem}
+
+  const handleTime = () =>{
+    alert(showDate(new Date()))
+  }
+
+  const greetPeople = () => {
+    alert('Bem-vindo ao desafio 30 dias de react')
+  }
+
+  return(
+    <div className='app'>
+      <Header data={data} />
+      <Main
+        user={user}
+        techs={techs}
+        handleTime={handleTime}
+        greetPeople={greetPeople}
+        />
+        <Footer copyRigth={date} />
+    </div>
+  )
+  
+}
 
 const rootElement = document.getElementById('root')
-// we render the JSX element using the ReactDOM package
-ReactDOM.render(app, rootElement)
+
+ReactDOM.render(<App />, rootElement)
