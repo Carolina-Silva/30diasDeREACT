2d28f3ca87dccec57132b34f5ef5d686d089663f
Carolina Silva <carol_paiva@outlook.com>
2023-04-12 16:52:43 -0300

    add exercicios dia07 Nivel 01

--- 30-days-of-react/src/index.js
+++ 30-days-of-react/src/index.js
@@ -1,39 +1,177 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const countries = [
-  { name: 'Finland', city: 'Helsinki' },
-  { name: 'Sweden', city: 'Stockholm' },
-  { name: 'Denmark', city: 'Copenhagen' },
-  { name: 'Norway', city: 'Oslo' },
-  { name: 'Iceland', city: 'Reykjavík' },
-]
+import userImg from './img/img.png'
 
-const Country = ({country: {name, city}}) => {
-  return(
-    <div>
-    <h1>{name}</h1>
-    <small>{city}</small>
-  </div>
-  )
-  }
-
-const Countries = ({ countries }) => {
-  const countryList = countries.map((country) => (
-    <Country key={country.name} country={country} />
-  ))
-  return <div>{countryList}</div>
-}
-
-const App = () =>(
-  <div className='container'>
-   <div>
-     <h1>Lista de países</h1>
-     < Countries countries={countries} />
-   </div>
+const UserCard = ({user: {firstName, lastName, image}}) => (
+  <div className='user-card'>
+    <img src={image} alt={firstName} />
+    <h2>
+      {firstName}
+      {lastName}
+    </h2>
   </div>
 )
 
-const rootElement = document.getElementById('root')
+const Button = ({text, onClick, style}) => (
+  <button style={style} onClick={onClick}>
+    {text}
+  </button>
+)
 
+const buttonStyles = {
+  backgroundColor: '#61dbfb',
+  padding: 10,
+  border: 'none',
+  borderRadius: 5,
+  margin: 3,
+  cursor: 'poiter',
+  fontSize: 18,
+  color:'white',
+}
+
+
+class Header extends React.Component {
+  greetPeople = () =>{
+    alert('Parabéns você chagou até o dia 07')
+  }
+  render() {
+    console.log(this.props.data)
+    const {
+      welcome,
+      title,
+      subtitle,
+      author: { firstName, lastName },
+      date,
+    } = this.props.data
+
+    return (
+      <header>
+        <div className='header-wrapper'>
+          <h1>{welcome}</h1>
+          <h2>{title}</h2>
+          <h3>{subtitle}</h3>
+          <p>
+            {firstName} {lastName}
+          </p>
+          <small>{date}</small>
+        </div>
+      </header>
+    )
+  }
+}
+
+
+class TechList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const { techs } = this.props
+    const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+    return techsFormatted
+  }
+}
+
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <main>
+        <div className='main-wrapper'>
+          <p>Prerequisite to get started react.js:</p>
+          <ul>
+            <TechList techs={this.props.techs} />
+          </ul>
+          <UserCard user={this.props.user}/>
+          <Button 
+            text= 'Greet people'
+            onClick ={this.props.greetPeople}
+            style = {buttonStyles}
+          />  
+        </div>
+      </main>
+    )
+  }
+}
+
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <footer>
+        <div className='footer-wrapper'>
+          <p>Copyright {this.props.date.getFullYear()}</p>
+        </div>
+      </footer>
+    )
+  }
+}
+
+class App extends React.Component {
+  showDate = (time) => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ]
+
+    const month = months[time.getMonth()].slice(0, 3)
+    const year = time.getFullYear()
+    const date = time.getDate()
+    return ` ${month} ${date}, ${year}`
+  }
+  handleTime = () => {
+    alert(this.showDate(new Date()))
+  }
+  greetPeople = () => {
+    alert('Welcome to 30 Days Of React Challenge, 2020')
+  }
+  render() {
+    const data = {
+      welcome: 'Welcome to 30 Days Of React',
+      title: 'Getting Started React',
+      subtitle: 'JavaScript Library',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+      date: 'Oct 7, 2020',
+    }
+    const techs = ['HTML', 'CSS', 'JavaScript']
+
+    const user = { ...data.author, image: userImg}
+
+    return (
+      <div className='app'>
+        <Header data={data} />
+        <Main
+          user={user}
+          techs={techs}
+          handleTime={this.handleTime}
+          greetPeople={this.greetPeople}
+        />
+
+        <Footer date={new Date()} />
+      </div>
+    )
+  }
+}
+
+const rootElement = document.getElementById('root')
 ReactDOM.render(<App />, rootElement)
