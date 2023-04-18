122fb4c6af427bfd8141db062032d183788f0209
Carolina Silva <carol_paiva@outlook.com>
2023-04-18 15:10:32 -0300

    interface projeto dia28

--- Dia28/copy-twitter/src/index.js
+++ Dia28/copy-twitter/src/index.js
@@ -1,17 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import arrowRepeat from './images/arrow-repeat.svg'
+import heart from './images/heart.svg'
+import pencilSquare from './images/pencil-square.svg'
+import personCircle from './images/person-circle.svg'
+import trash from './images/trash.svg'
+import twitter from './images/twitter.svg'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Header = () => (
+  <header>
+    <div className="header">
+      <img src={twitter} />
+    </div>
+  </header>
+)
+
+const InputTweet = () =>(
+  <div className="input-tweet-box">
+    <input 
+    type="text" 
+    name="input-tweet" 
+    id="input-tweet"
+    placeholder='Escreva sobre o Desafio React' 
+    />
+    <button className='btn-tweet-post'> Add post</button>
+  </div>
+)
+
+const Tweet = () =>(
+  <div className="tweet-card">
+    <div className="tweet-header">
+      <img src={personCircle}/>
+      <h5>Nome User</h5>
+      <small>@user</small>
+    </div>
+    <div className="tweet-main">
+      <p>
+      30 Days Of React challenge has been started on 1 October and still ongoing. It will end the 30 October 2020. It was a real challenge for everyone. Students learned quite a lot of concepts. I hope this will help lots of students. 
+      </p>
+    </div>
+    <div className="tweet-footer">
+      <div className='edit'>
+        <a href="/">
+          <img src={pencilSquare} />
+        </a>
+        <a href="/">
+          <img src={trash} />
+        </a>
+      </div>
+      <div className='like'>
+          <img src={heart} />
+          <img src={heart} />
+          <img src={arrowRepeat} />
+      </div>
+      <div className='date'>
+        <small>Apr 18, 2023 14:29</small>
+      </div>
+    </div>
+  </div>
+)
+
+const Main = () => (
+  <main>
+    <InputTweet />
+    <Tweet />
+    <Tweet />
+    <Tweet />
+    <Tweet />
+    
+  </main>
+)
+
+const App = () => (
+  <div className='app'>
+    <Header/>
+    <Main />
+  </div>
+)
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(<App />, rootElement)
