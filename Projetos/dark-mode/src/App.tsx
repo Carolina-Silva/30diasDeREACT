70e6d4efcf1c07f0cc62d6dcb44ef0fe012bdc2c
Carolina Silva <carol_paiva@outlook.com>
2023-04-20 15:14:33 -0300

    add card ao projeto dark mode

--- Projetos/dark-mode/src/App.tsx
+++ Projetos/dark-mode/src/App.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import DarkMode from './DarkMode';
+
+
+const Header = () => (
+  <header>
+    <div className="logo">
+      <h1>LOGO</h1>
+    </div>
+    <div className="toggle-theme-box">
+        <DarkMode />
+    </div>
+  </header>
+)
+
+const CardUser = () => {
+  return(
+    <div className="card-user">
+    <div className="card-header">
+      <img src="https://avatars.githubusercontent.com/u/65463404?s=96&v=4" alt=" Imagem do usuario" />
+      <h3>Carolina Silva</h3>
+    </div><div className="card-main">
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. I</p>
+      <a href="/">
+          <i className="bi bi-github"></i>
+      </a>
+      <a href="/">
+        <i className="bi bi-linkedin"></i>
+      </a>
+    </div>
+  </div>
+
+  )
+  
+
+}
+
+const Main = () => (
+  <main>
+    <CardUser /> 
+  </main>
+)
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Main />
+          
     </div>
   );
 }
